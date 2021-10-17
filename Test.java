5e45ac3abea83da12f2da876adecaf0222f9b8f3
Danny Luu <luudanny@hawaii.edu>
2021-10-16 20:26:40 -1000

    Outline

---
+++ Test.java
@@ -0,0 +1,201 @@
+/**
+*Description of Program.
+*@author Danny Luu
+*@since __/__/__
+*/
+
+public class Test {
+
+   /**
+   *main method.
+   *@param args not used
+   */
+   public static void main(String[] args) {
+/*
+      player p1 = new player(name1, poke)
+      player p2 = new player(name2, poke)
+      
+      
+      player playerOne 
+      player playerTwo
+      Player tempPlayer
+      
+      number = random ( range = 2)
+      if ( n == 0)
+         playerOne = p1
+         playerTwo = p2
+      else 
+         playerOne = p2
+         playerTwo = p1
+      
+      while (hp){
+         
+         playerOne.name() , it is your turn! 
+         Your HP: 56
+         Your attack energy: 1
+         Player2's HP: 129
+         tempPlay = battle(p1, p2)
+         
+         
+      if tempplay.anme() = playerOne.name()
+         playerOne = tempplay
+      else
+         playerTwo = tempplay
+      player 2 battle
+            battle 
+       
+       counter ++
+      }
+      
+      tempPlay = battle(p1, p2)
+      
+      if
+         tempplay.anme() = p1.name()
+      else
+         tempplay.anme() = p2.name()
+   
+   } // Closes main method
+   
+   public static player BATTLE(player play1 play2) {
+       
+         What do you want to do?
+         1. pass to build energy
+         2. perform fastAttack
+         if (energy = 3)
+         3. perform specialAttack
+         else 
+         
+         switch
+            1
+               playerOne.gainEnergy()
+            2
+               a = 1
+               play = damage (p1, p2 , a)
+            3
+               a = 2
+               play = damage (p1, p2 , a)
+            
+        return play
+   }
+   
+   public static player Damage(player play1 play2 int a) {
+       
+       if (a = 1)
+       player.chooseFastAttack(opponent)
+       
+       else
+       player.chooseSpecialAttack(opponent)
+       
+       
+       if (opponent hp <= 0)
+         oppenetn pokemon fainted
+       else 
+         "oppent hp left"
+       
+       return play(opponent) 
+   }
+
+*/  
+   
+} // Closes class
+
+/*
+      
+      System.out.println("Player 1, please enter your name");
+      name = userIn.nextLine();
+      name = name1.trim();
+      System.out.println("Player 1 set to " name1);
+      
+      // Calls choosePokemon method
+      poke = choosePokemon();
+      
+      player p1 = new player(name1, poke)
+      
+      System.out.println("Player 2");
+
+      
+      // Calls choosePokemon method
+      poke = choosePokemon();
+      
+      player p2 = new player(name1, poke)
+      
+      
+      
+      */
+      
+
+   
+   
+   
+   
+   /*
+   //============= choosePokemon ==================
+   /**
+   * Creates a pokemon object from a selection of 9.
+   * @return a Pokemon object
+   
+   public static Pokemon choosePokemon() {
+      Pokemon poke = new Bulbasaur();
+      Scanner userIn = new Scanner(System.in);
+      int choiceNum = 0;
+      String choice = new String("");
+      boolean endLoop = true;
+      do {
+         // menu text
+         System.out.println("Please enter a number to choose your Pokemon:");
+         System.out.println("\t1 for Bulbasaur");
+         System.out.println("\t2 for Ivysaur");
+         System.out.println("\t3 for Venusaur");
+         System.out.println("\t4 for Charmander");
+         System.out.println("\t5 for Charmeleon");
+         System.out.println("\t6 for Charizard");
+         System.out.println("\t7 for Squirtle");
+         System.out.println("\t8 for Wartortle");
+         System.out.println("\t9 for Blastoise");
+         
+         //Reads user's choice
+         choice = userIn.nextLine();
+         choice = choice.trim();
+      
+         switch (choice) {
+            case "1":
+                  poke = new Bulbasaur();
+                  break;
+            case "2":
+                  poke = new Ivysaur();
+                  break;
+            case "3":
+                  poke = new Venusaur();
+                  break;
+            case "4":
+                  poke = new Charmander();
+                  break;
+            case "5":
+                  poke = new Charmeleon();
+                  break;
+            case "6":
+                  poke = new Charizard();
+                  break;
+            case "7":
+                  poke = new Squirtle();
+                  break;
+            case "8":
+                  poke = new Wartortle();
+                  break;
+            case "9":
+                  poke = new Blastoise();
+                  break;
+            default:
+                  endLoop = false;
+                  System.out.println("\n****Invalid menu choice.****");
+                  System.out.println("Please enter a number from 1 to 9\n");
+                  break; 
+         } // Closes Switch
+      } while (!endLoop);
+      return poke;
+   } // Closes makePokemon
+   */
+   
+   
+} 
+
