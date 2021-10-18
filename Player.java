729c6402e3250d6af65da88a6ba71884f2dd820a
Danny Luu <luudanny@hawaii.edu>
2021-10-17 14:51:25 -1000

    Player in progress

--- Player.java
+++ Player.java
@@ -1,24 +1,85 @@
 /**
 * Player object class.
-* @author Lisa Miller
+* @author Danny Luu
 * @since 10/17/2021
 */
 public class Player {
+   
+   //============= Instance Variables ==================
+   /** The Player's Name. */
+   private String name = "";
+   /** The Player's Pokemon*/
+   private Pokemon poke = new Bulbasaur();
+   /** The Player's Energy */
+   private int energy = 3;
 
-/**
+   /**
    * Constructor for Player.
    */
-   public Player(String name, Pokemon poke) {
-      super("Bulbasaur", "Bulbasaur", 1, GRASS_COLOR, 0.71, 6.9, 
-        GRASS_TYPE, POISON_TYPE, BASE_ATTACK_POWER, 
-        BASE_DEFENSE_POWER, BASE_STAMINA_POWER);   
-      //pick Attacks after construct
-      chooseFastAttack();
-      chooseSpecialAttack();
+   public Player(String newName, Pokemon newPoke) {
+       this.name = newName;
+       this.poke = newPoke;
+       this.energy = energy;
    }
+   
+   /**
+   * The player performs a fast attack
+   * @param 
+   * @return 
+   */
+   public Player fastAttack(Player opponent){
+      String s = "";
+      Player p = opponent;
+      s = this.poke.performFastAttack(p.getPoke());
+      System.out.println(s);
+      return p;
+   }
+   
+   /**
+   * Gives the player one energy when they pass.
+   * @return int the Player's new energy
+   */
+   public int passTurn(){
+      this.energy = energy++;
+      return this.energy;
+   }
+   
+   
 
 
-
+   //============= getMethod ==================
+   /**
+   * Gets the Player's name.
+   * @return String the Player's name
+   */
+   public String getName(){
+      return this.name;
+   }
+   
+   /**
+   * Gets the Player's pokemon.
+   * @return Pokemon the Player's pokemon
+   */
+   public Pokemon getPoke(){
+      return this.poke;
+   }
+   
+   /**
+   * Gets the Player's energy.
+   * @return int the Player's energy
+   */
+   public int getEnergy(){
+      return this.energy;
+   }
+   
+   /**
+   * Gets the Player's Pokemon's Hp.
+   * @return int the Player's energy
+   */
+   public int getHp(){
+      return this.poke.getHP();
+   }
+   
 
 
 }
